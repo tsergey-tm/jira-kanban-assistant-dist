f4b30cb6a20fe415962b2c02471f3166f3e391f7
Sergey Tarabukin <sergey.tarabukin@gmail.com>
2023-03-17 12:07:53 +0500

    New version

--- assets/index-2cdc98d9.js
+++ assets/index-2cdc98d9.js
@@ -66729,7 +66729,7 @@ const messages = {
       "help": {
         "link": (ctx) => {
           const { normalize: _normalize } = ctx;
-          return _normalize(["lead-cycle-times-chart.help.link"]);
+          return _normalize(["https://github.com/tsergey-tm/jira-kanban-assistant-dist/blob/master/docs/plugin-doc.en.md#lead--cycle-times"]);
         },
         "text": (ctx) => {
           const { normalize: _normalize } = ctx;
@@ -67909,7 +67909,7 @@ const messages = {
       "help": {
         "link": (ctx) => {
           const { normalize: _normalize } = ctx;
-          return _normalize(["lead-cycle-times-chart.help.link"]);
+          return _normalize(["https://github.com/tsergey-tm/jira-kanban-assistant-dist/blob/master/docs/plugin-doc.ru.md#%D0%B2%D1%80%D0%B5%D0%BC%D1%8F-%D0%BF%D0%BE%D1%81%D1%82%D0%B0%D0%B2%D0%BA%D0%B8-%D0%B8-%D1%86%D0%B8%D0%BA%D0%BB%D0%B0"]);
         },
         "text": (ctx) => {
           const { normalize: _normalize } = ctx;
